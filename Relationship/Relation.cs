68b15f19268df4883e31c4dc61f50440f76e76cf
jakob arndal <takana@exelion.dk>
2016-03-12 19:25:40 +0100

    First Commit
    Alt hvad hjertet kunne begære

---
+++ Relationship/Relation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relations
+{
+    class Relation<T>
+    {
+        T owner { get; } 
+        T other { get; }
+        //bool tradeAgreement { get; set; }
+
+        float relationship { get; set; }
+        /// <summary>
+        /// This Class can be used to make relationships between instances of object. You will have to define what type of object these relationships can be
+        /// coupled to.
+        /// </summary>
+        /// <param name="own"></param>
+        /// <param name="oth"></param>
+
+        public Relation()
+        {
+
+        }
+        public Relation(T own, T oth)
+        {
+            owner = own;
+            other = oth;
+            relationship = 0;
+        }
+        public Relation(T own, T oth, bool BothWays) {
+            owner = own;
+            other = oth;
+            relationship = 0;
+           
+
+        }
+
+        public T GetOther() { return other; }
+        public T GetOwner() { return owner; }
+        public float GetRelationship()
+        {
+            return relationship;
+        }
+        public void SetRelationship(float chng)
+        {
+            relationship += chng;
+            OnRelationshipChange(relationship, owner, other);
+
+        }
+
+        // need the current relationship and
+        public delegate void RelationshipChanged(float relationship, T Owne, T Othe);
+        public event RelationshipChanged OnRelationshipChange;
+
+        
+    }
+}
---
+++ Relationship/RelationshipController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relations
+{
+
+
+    class RelationshipController<T>
+    {
+        private static volatile RelationshipController<T> inst;
+        public static RelationshipController<T> GetRelaCont() {
+            if (inst == null)
+            {
+                inst = new RelationshipController<T>();
+            }
+            return inst;
+        }
+       
+
+
+        List<Relation<T>> IngameRelations;
+
+        public RelationshipController() {
+            IngameRelations = new List<Relation<T>>();
+        }
+
+         void AddRelationship(Relation<T> rela) {
+            IngameRelations.Add(rela);
+            rela.OnRelationshipChange += ListenForChange;
+        }
+
+        public void RemoveRelationship(Relation<T> rela)
+        {
+            IngameRelations.Remove(rela);
+            rela.OnRelationshipChange -= ListenForChange;
+        }
+
+        public void CreateRelation(T own, T oth) {
+            Relation<T> r = new Relation<T>(own, oth);
+            AddRelationship(r);
+
+        }
+
+        public void ChangeRelationship(T own, T oth, float chng) {
+            IngameRelations.Find(x => x.GetOwner().Equals(own) && x.GetOther().Equals(oth)).SetRelationship(chng);
+        }
+
+
+        void ListenForChange(float relation, T owne, T othe) {
+
+            OnRelationshipChange(relation, owne, othe);
+
+        }
+
+        public delegate void BroadcastChange(float relationship, T Owne, T Othe);
+        public event BroadcastChange OnRelationshipChange;
+
+
+
+
+
+
+
+    }
+}
