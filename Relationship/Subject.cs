960f7a8fc157d5227ecfb4c2d0fcc4adeeb28aa4
pareion <pareion12@gmail.com>
2016-03-13 14:48:05 +0100

    Forgot these

---
+++ Relationship/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relations
+{
+    public class Subject
+    {
+        public float relationship { get; set; }
+        public float getState()
+        {
+            return relationship;
+        }
+        public void setState(float relationship)
+        {
+            this.relationship = relationship;
+        }
+    }
+}
