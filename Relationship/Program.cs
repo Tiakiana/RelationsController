f96241e2f741bb6b47c33b7c3d51b0b9eaa9550a
Tiakiana <takana@exelion.dk>
2016-10-18 11:57:49 +0200

    Now with explanation

--- Relationship/Program.cs
+++ Relationship/Program.cs
@@ -8,29 +8,37 @@ namespace Relations
 {
     class Program
     {
-        //This is the implementation of it.
+        //This is what we thinkt the party should do with the information
         public void OnChange(float relationship, string Owne, string Othe) {
-            if (true)
+            if (relationship< -10)
             {
+                Console.WriteLine(Owne + " Declares war on " + Othe);
+            }
+            if (relationship>10)
+            {
+                Console.WriteLine(Owne + " allies with " + Othe);
 
             }
         }
+        // This class is only for example
         static void Main(string[] args)
         {
+            //First up we instantiate the relationship controller with the type of
+            //thing that should have relations.
+            //this could be a class or in this case just a string.
             RelationshipController<string> rc = RelationshipController<string>.GetRelaCont();
+            //We here make a pseudo program
             Program p = new Program();
 
-
+            //Here we create the relation between 2 strings
             rc.CreateRelation("Denmark", "USA");
+            //then we subscribe what should be done once the relationship changes
             rc.OnRelationshipChange += p.OnChange;
 
-            rc.ChangeRelationship("Denmark", "USA", -10);
-            rc.ChangeRelationship("Denmark", "USA", -20);
+            //Then Denmark discovers USA's secret plans to invade Greenland
+            rc.ChangeRelationship("Denmark", "USA", -11);
             
-            rc.CreateRelation("Germany", "USA");
-            rc.CreateRelation("Sweden", "USA");
-            rc.ChangeRelationship("Denmark", "USA", 5);
-           
+           //Boom nuclear war is emminent!
             Console.Read();
         }
     }
